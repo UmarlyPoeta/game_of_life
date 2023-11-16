4e358900640384ebfbe9f0da425ecf1672606a47
Patryk <patryk.kozlowski1104@gmail.com>
2023-11-16 18:26:49 +0100

    added additional features

--- gui_game_of_life.py
+++ gui_game_of_life.py
@@ -2,59 +2,60 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-#list of options where "0"=alive cell and "."=dead cell
 list_of_options = ["0", "."]
-row_number=6
-column_number=6
+row_number = 6
+column_number = 6
 
 class GameOfLife:
     def __init__(self):
-        """
-        init function that declares randomly generated board with values "0" meaning alive cell and 
-        "." meaning dead cell. Function also declares tkinter widgets like buttons, canvas and frame
-        
-        """
-        self.board = [[random.choice(list_of_options) for _ in range(column_number)] for _ in range(row_number)] #randomly generated board
-        self.how_many_iterations = 0 #number of iterations
-        self.how_many_alive = [] #numbers of alive cells at iteration number index
-        self.how_many_alive.append(self.chech_how_many_alive()) #adds 0 iteration index value of alive cells
+        self.board = [[random.choice(list_of_options) for _ in range(column_number)] for _ in range(row_number)]
+        self.how_many_iterations = 0
+        self.how_many_alive = []
+        self.how_many_alive.append(self.chech_how_many_alive())
 
-        #root
         self.root = tk.Tk()
         self.root.title("Game of Life")
 
-        #frame
-        self.canvas_frame = tk.Frame(self.root) 
+        self.canvas_frame = tk.Frame(self.root)
         self.canvas_frame.pack(side=tk.TOP)
 
-        #canvas
-        self.canvas = tk.Canvas(self.canvas_frame, width=300, height=300) 
+        self.canvas = tk.Canvas(self.canvas_frame, width=300, height=300)
         self.canvas.pack()
 
-        #board button
         self.show_board_button = tk.Button(self.root, text="Show Board", command=self.show_board)
         self.show_board_button.pack(side=tk.LEFT)
 
-
-        #do 1 iteration button
-        self.one_iteration_button = tk.Button(self.root, text="Do 1 Iteration", command=self.do_one_iteration)
+        self.one_iteration_button = tk.Button(self.root, text="Do Iterations", command=self.do_one_iteration)
         self.one_iteration_button.pack(side=tk.LEFT)
 
-        
-        #show evolution button
         self.show_evolution_button = tk.Button(self.root, text="Show Evolution", command=self.make_a_plot)
         self.show_evolution_button.pack(side=tk.LEFT)
 
+        self.clear_board_button = tk.Button(self.root, text="Clear Board", command=self.clear_board)
+        self.clear_board_button.pack(side=tk.LEFT)
 
-        #exit button
+        self.random_fill_button = tk.Button(self.root, text="Random Fill", command=self.random_fill)
+        self.random_fill_button.pack(side=tk.LEFT)
+
+        self.save_board_button = tk.Button(self.root, text="Save Board", command=self.save_board)
+        self.save_board_button.pack(side=tk.LEFT)
+
+        self.load_board_button = tk.Button(self.root, text="Load Board", command=self.load_board)
+        self.load_board_button.pack(side=tk.LEFT)
+
+        self.change_size_button = tk.Button(self.root, text="Change Size", command=self.change_size)
+        self.change_size_button.pack(side=tk.LEFT)
+
+        self.speed_scale = tk.Scale(self.root, label="Animation Speed", from_=1, to=10, orient=tk.HORIZONTAL)
+        self.speed_scale.set(5)
+        self.speed_scale.pack(side=tk.LEFT)
+
         self.exit_button = tk.Button(self.root, text="Exit", command=self.end)
         self.exit_button.pack(side=tk.LEFT)
 
     def show(self):
-        """
-        function that prints board
-        """
         for row in self.board:
             print(" ".join(row))
         print()
@@ -63,14 +64,9 @@ class GameOfLife:
         self.draw_board()
 
     def end(self):
-        """
-        function that exits program
-        """
         self.root.destroy()
 
     def make_a_plot(self):
-        """Function that makes a graph based on number of iteration and number of alive cells
-        """
         x = np.array([i for i in range(0, self.how_many_iterations + 1)])
         y = np.array(self.how_many_alive)
         plt.scatter(x, y)
@@ -80,15 +76,6 @@ class GameOfLife:
         plt.show()
 
     def how_many_neighbours(self, column_index, row_index):
-        """function checks how many neighbours each cell have
-
-        Args:
-            column_index (int): cell column index
-            row_index (int): cell row index
-
-        Returns:
-            int: number of neighbours
-        """
         neighbours = 0
         if column_index - 1 >= 0:  # left
             if self.board[row_index][column_index - 1] == "0":
@@ -117,11 +104,6 @@ class GameOfLife:
         return neighbours
 
     def chech_how_many_alive(self):
-        """function that returns how many alive cells are in each iteration
-
-        Returns:
-            int: number of alive cells in iteration
-        """
         n = 0
         for i in self.board:
             for j in i:
@@ -130,14 +112,9 @@ class GameOfLife:
         return n
 
     def one_iteration(self):
-        """function that does one iteration,checks how many neighbours each cell have and based
-        on that change them based on game criteria
-        """
         for row_index, row in enumerate(self.board):
             for column_index, column in enumerate(row):
-                # checking how many neighbours
                 neighbours = self.how_many_neighbours(column_index, row_index)
-                # is it dead or alive
                 if column == "0":
                     if neighbours < 2:
                         self.board[row_index][column_index] = "."
@@ -150,32 +127,75 @@ class GameOfLife:
         self.how_many_iterations += 1
 
     def do_one_iteration(self):
-        """function that does one iteration and shows the gui board
-        """
-        self.one_iteration()
-        self.show_board()
+        speed = self.speed_scale.get()
+        for _ in range(speed):
+            self.one_iteration()
+            self.draw_board()
+            self.root.update()
+            self.root.after(100)  # Pause for a short while between iterations
 
     def draw_board(self):
-        """function that draws board
-        """
         self.canvas.delete("all")
         tile_size = 50
         for row_index, row in enumerate(self.board):
-            for column_index, column in enumerate(row):
-                x1 = column_index * tile_size
-                y1 = row_index * tile_size
-                x2 = x1 + tile_size
-                y2 = y1 + tile_size
-                color = "green" if column == "0" else "red"
-                self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="black")
+                for column_index, column in enumerate(row):
+                    x1 = column_index * tile_size
+                    y1 = row_index * tile_size
+                    x2 = x1 + tile_size
+                    y2 = y1 + tile_size
+                    color = "green" if column == "0" else "red"
+                    self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="black")
+
+    def clear_board(self):
+        self.board = [["." for _ in range(column_number)] for _ in range(row_number)]
+        self.how_many_iterations = 0
+        self.how_many_alive = []
+        self.how_many_alive.append(self.chech_how_many_alive())
+        self.draw_board()
+
+    def random_fill(self):
+        self.board = [[random.choice(list_of_options) for _ in range(column_number)] for _ in range(row_number)]
+        self.how_many_iterations = 0
+        self.how_many_alive = []
+        self.how_many_alive.append(self.chech_how_many_alive())
+        self.draw_board()
+
+    def save_board(self):
+        with open("saved_board.txt", "w") as file:
+            for row in self.board:
+                file.write(" ".join(row) + "\n")
+
+    def load_board(self):
+        try:
+            with open("saved_board.txt", "r") as file:
+                loaded_board = [list(line.strip()) for line in file]
+            if len(loaded_board) == row_number and all(len(row) == column_number for row in loaded_board):
+                self.board = loaded_board
+                self.how_many_iterations = 0
+                self.how_many_alive = []
+                self.how_many_alive.append(self.chech_how_many_alive())
+                self.draw_board()
+            else:
+                print("Invalid file format.")
+        except FileNotFoundError:
+            print("No saved board found.")
+
+    def change_size(self):
+        global row_number, column_number
+        new_row_number = int(input("Enter the new number of rows: "))
+        new_column_number = int(input("Enter the new number of columns: "))
+        self.board = [[random.choice(list_of_options) for _ in range(new_column_number)] for _ in range(new_row_number)]
+        row_number, column_number = new_row_number, new_column_number
+        self.how_many_iterations = 0
+        self.how_many_alive = []
+        self.how_many_alive.append(self.chech_how_many_alive())
+        self.draw_board()
 
     def run_gui(self):
-        """function that runs mainloop
-        """
         self.root.mainloop()
 
 def main():
-    game=GameOfLife()
+    game = GameOfLife()
     game.run_gui()
 
 if __name__ == "__main__":
