dccd2754839e90f139a5d398e64cc29096b9a76a
Patryk <patryk.kozlowski1104@gmail.com>
2023-11-16 01:31:07 +0100

    made some changes and add gui show board interpre

--- gui_game_of_life.py
+++ gui_game_of_life.py
@@ -1,6 +1,4 @@
 import random
-import sys
-import os
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
@@ -15,13 +13,19 @@ class GameOfLife:
         self.how_many_iterations = 0
         self.how_many_alive = []
         self.how_many_alive.append(self.chech_how_many_alive())
-        
+
         self.root = tk.Tk()
         self.root.title("Game of Life")
 
+        self.canvas_frame = tk.Frame(self.root)
+        self.canvas_frame.pack(side=tk.TOP)
+
+        self.canvas = tk.Canvas(self.canvas_frame, width=300, height=300)
+        self.canvas.pack()
+
         self.show_board_button = tk.Button(self.root, text="Show Board", command=self.show_board)
         self.show_board_button.pack(side=tk.LEFT)
-        
+
         self.one_iteration_button = tk.Button(self.root, text="Do 1 Iteration", command=self.do_one_iteration)
         self.one_iteration_button.pack(side=tk.LEFT)
 
@@ -32,24 +36,23 @@ class GameOfLife:
         self.exit_button.pack(side=tk.LEFT)
 
     def show(self):
-        a = np.array(self.board)
-        print(str(a.reshape(6, 6)).replace("[", "").replace("]", "").replace("'", " "))
+        for row in self.board:
+            print(" ".join(row))
+        print()
 
     def show_board(self):
-        self.show()
+        self.draw_board()
 
     def end(self):
-        self.root.destroy
-        sys.exit()
-    
+        self.root.destroy()
+
     def make_a_plot(self):
-        x=np.array([i for i in range(0,self.how_many_iterations+1)])
-        y=np.array(self.how_many_alive)
+        x = np.array([i for i in range(0, self.how_many_iterations + 1)])
+        y = np.array(self.how_many_alive)
         plt.scatter(x, y)
-        plt.title("plot")
-        plt.xlabel("iteration number")
-        plt.ylabel("number of alive cells")
-        plt.legend()
+        plt.title("Plot")
+        plt.xlabel("Iteration number")
+        plt.ylabel("Number of alive cells")
         plt.show()
 
     def how_many_neighbours(self, column_index, row_index):
@@ -107,7 +110,19 @@ class GameOfLife:
 
     def do_one_iteration(self):
         self.one_iteration()
-        self.make_a_plot()
+        self.show_board()
+
+    def draw_board(self):
+        self.canvas.delete("all")
+        tile_size = 50
+        for row_index, row in enumerate(self.board):
+            for column_index, column in enumerate(row):
+                x1 = column_index * tile_size
+                y1 = row_index * tile_size
+                x2 = x1 + tile_size
+                y2 = y1 + tile_size
+                color = "green" if column == "0" else "red"
+                self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="black")
 
     def run_gui(self):
         self.root.mainloop()
